fbbd2e286e19ef39bcc019795d07881bf971b7e3
Habilidebob de Jesus <80491104+habilidebob@users.noreply.github.com>
2022-02-24 15:34:16 -0300

    remoção da foto

--- sisapi/api/cadastraProduto.php
+++ sisapi/api/cadastraProduto.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['infosusuario'])) {
 // Verificar se a pessoa está logada:
 
 // Puxar o arquivo de conexão com o banco de dados:
-include('../db/banco.php');
+include('db/banco.php');
 
 // Definir fuso horário:
 date_default_timezone_set('America/Sao_Paulo');
@@ -48,22 +48,8 @@ $categoria = $_POST['categoria'];
 // Obter o ID do usuário pela sessão atual:
 $idResp = $_SESSION['infosusuario']['idUsuario'];
 
-// Upload de Arquivos:
+$foto = "fotos/semfoto.jpg";
 
-// Valor aleatório: rand(inicial,final);
-// 20211214160617_XXXX
-$novoNome = date('YmdHis') . "_" . rand(1000, 9999);
-// Extrair a extensão do arquivo enviado:
-$ext = substr($_FILES['foto']['name'], -4);
-// Definir o novo nome do arquivo com a extensão:
-$novoNome = $novoNome . $ext;
-
-// Mover e verificar se deu certo:
-if (move_uploaded_file($_FILES['foto']['tmp_name'], "fotos/" . $novoNome)) {
-    $foto = "fotos/" . $novoNome;
-} else {
-    $foto = "fotos/semfoto.jpg";
-}
 
 // Caso a foto não esteja definida, setar para fotos/semfoto.jpg
 try {
